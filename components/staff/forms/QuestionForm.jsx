a96eec123534bf56914df2b1e29b2c39fd43a221
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-02-04 10:05:25 +0300

    fix question form

--- components/staff/forms/QuestionForm.jsx
+++ components/staff/forms/QuestionForm.jsx
@@ -3,15 +3,23 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
-import {  questionSchema } from '@/lib/schemas';
+import { questionSchema } from '@/lib/schemas';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import {  createQuestion, getDifficultyLevels, getObjectivesQuiz, getSubjects, getSubtopics, getTopics,  updateQuestion } from '@/lib/actions/Staff';
+import {
+  createQuestion,
+  getDifficultyLevels,
+  getObjectivesQuiz,
+  getSubjects,
+  getSubtopics,
+  getTopics,
+  updateQuestion,
+} from '@/lib/actions/Staff';
 import { useToast } from '@/hooks/use-toast';
 import CustomFormField from '@/components/forms/CustomFormField';
 import { Form } from '@/components/ui/form';
 import { useEffect, useState } from 'react';
 import { FormFieldTypes } from '@/constants';
-const QuestionForm = ({ questionToEdit = {} }) => {
+const QuestionForm = ({ questionToEdit = {}, setOpen }) => {
   const { toast } = useToast();
   const [selectedSubject, setSelectedSubject] = useState(null);
   const [selectedTopic, setSelectedTopic] = useState(null);
@@ -21,12 +29,12 @@ const QuestionForm = ({ questionToEdit = {} }) => {
   const isEditSession = Boolean(editId);
 
   const { data: subjects } = useQuery({
-    queryKey: ["subjects"],
+    queryKey: ['subjects'],
     queryFn: () => getSubjects({ pageIndex: 0 }).then((res) => res.results),
   });
 
   const { data: topics } = useQuery({
-    queryKey: ["topics", selectedSubject],
+    queryKey: ['topics', selectedSubject],
     queryFn: () =>
       selectedSubject
         ? getTopics({ pageIndex: 0, subject_id: selectedSubject }).then(
@@ -35,11 +43,9 @@ const QuestionForm = ({ questionToEdit = {} }) => {
         : [],
     enabled: !!selectedSubject,
   });
-  console.log("selected Subject", selectedSubject);
-
 
   const { data: subtopics } = useQuery({
-    queryKey: ["subtopics", selectedTopic],
+    queryKey: ['subtopics', selectedTopic],
     queryFn: () =>
       selectedTopic
         ? getSubtopics({ pageIndex: 0, topic_id: selectedTopic }).then(
@@ -50,13 +56,13 @@ const QuestionForm = ({ questionToEdit = {} }) => {
   });
 
   const { data: difficultyLevels } = useQuery({
-    queryKey: ["difficultyLevels"],
+    queryKey: ['difficultyLevels'],
     queryFn: () =>
       getDifficultyLevels({ pageIndex: 0 }).then((res) => res.results),
   });
 
   const { data: objectives } = useQuery({
-    queryKey: ["objectives", selectedTopic],
+    queryKey: ['objectives', selectedTopic],
     queryFn: () => {
       const params = { pageIndex: 0 };
       if (selectedTopic) {
@@ -76,18 +82,18 @@ const QuestionForm = ({ questionToEdit = {} }) => {
   }, [editValues, isEditSession]);
   const questionFormFields = [
     {
-      name: "subject",
-      label: "Subject",
-      placeholder: "PHYSICS",
+      name: 'subject',
+      label: 'Subject',
+      placeholder: 'PHYSICS',
       fieldType: FormFieldTypes.SELECT,
       selectItems: subjects,
       onChange: (selectedOption) =>
         setSelectedSubject(selectedOption ? selectedOption.value : null),
     },
     {
-      name: "topic",
-      label: "Topic",
-      placeholder: "ALGEBRA",
+      name: 'topic',
+      label: 'Topic',
+      placeholder: 'ALGEBRA',
       fieldType: FormFieldTypes.SELECT,
       selectItems: topics,
       onChange: (selectedOption) => {
@@ -96,47 +102,47 @@ const QuestionForm = ({ questionToEdit = {} }) => {
       },
     },
     {
-      name: "subtopic",
-      label: "Subtopic",
-      placeholder: "DIVISION",
+      name: 'subtopic',
+      label: 'Subtopic',
+      placeholder: 'DIVISION',
       fieldType: FormFieldTypes.SELECT,
       selectItems: subtopics,
       onChange: (selectedOption) =>
         setSelectedSubtopic(selectedOption ? selectedOption.value : null),
     },
     {
-      name: "difficulty_level",
-      label: "Difficulty Level",
-      placeholder: "MODERATE",
+      name: 'difficulty_level',
+      label: 'Difficulty Level',
+      placeholder: 'MODERATE',
       fieldType: FormFieldTypes.SELECT,
       selectItems: difficultyLevels,
     },
     {
-      name: "objective",
-      label: "Objective",
-      placeholder: "Solve the equation",
+      name: 'objective',
+      label: 'Objective',
+      placeholder: 'Solve the equation',
       fieldType: FormFieldTypes.SELECT,
       selectItems: objectives,
       onChange: (selectedOption) =>
         setSelectedObjective(selectedOption ? selectedOption.value : null),
     },
     {
-      name: "marks",
-      label: "Marks",
-      placeholder: "10",
-      inputType: "number",
+      name: 'marks',
+      label: 'Marks',
+      placeholder: '10',
+      inputType: 'number',
       fieldType: FormFieldTypes.INPUT,
     },
     {
-      name: "description",
-      label: "Description",
-      placeholder: "Describe the question...",
+      name: 'description',
+      label: 'Description',
+      placeholder: 'Describe the question...',
       fieldType: FormFieldTypes.TEXTAREA,
     },
     {
-      name: "marking_scheme",
-      label: "Marking Scheme",
-      placeholder: "Describe the marking scheme...",
+      name: 'marking_scheme',
+      label: 'Marking Scheme',
+      placeholder: 'Describe the marking scheme...',
       fieldType: FormFieldTypes.TEXTAREA,
     },
   ];
@@ -163,11 +169,12 @@ const QuestionForm = ({ questionToEdit = {} }) => {
       queryClient.invalidateQueries(['questions']);
       setOpen(false);
     },
-    onError: () => {
+    onError: (error) => {
       toast({
-        description: isEditSession
+        title: isEditSession
           ? 'Failed to update question.'
           : 'Failed to create question.',
+        description: error.message || 'Something went wrong!',
         variant: 'destructive',
       });
     },
@@ -190,13 +197,14 @@ const QuestionForm = ({ questionToEdit = {} }) => {
             iconSrc={field.iconSrc}
             iconAlt={field.iconAlt}
             selectItems={field.selectItems}
+            onChange={field.onChange}
           />
         ))}
         <SubmitButton />
       </form>
     </Form>
   );
-}
+};
 function SubmitButton() {
   const { pending } = useFormStatus();
   return (
@@ -209,4 +217,4 @@ function SubmitButton() {
     </Button>
   );
 }
-export default QuestionForm
+export default QuestionForm;
