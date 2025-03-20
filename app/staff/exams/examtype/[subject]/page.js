c1d9e81f9fb9b90cfac25b1771983dd749ef3674
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-03-20 15:23:03 +0300

    final fix

--- app/staff/exams/examtype/[subject]/page.js
+++ app/staff/exams/examtype/[subject]/page.js
@@ -1,10 +1,11 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
-import { getSubjectDetails } from '@/lib/actions/Staff';
+import { getSubjectDetails, getSubjectDetails_2 } from '@/lib/actions/Staff';
 import SubjectListView from '@/components/staff/SubjectListView';
 
 const SubjectListPage = ({ params }) => {
   const { subject } = params;
+  const rest_object = {subject_id: subject}
 
   const {
     data: subject_details,
@@ -31,9 +32,9 @@ const SubjectListPage = ({ params }) => {
       <SubjectListView
         isLoading={isLoading}
         data={subject_details}
-        fetchFunction={getSubjectDetails}
+        fetchFunction={getSubjectDetails_2}
         queryKey={subject}
-        rest={subject}
+        rest={rest_object}
       />
     </div>
   );
